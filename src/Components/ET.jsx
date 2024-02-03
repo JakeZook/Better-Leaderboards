82a6add0bb5ce08bc0bfd3616bbcaaf608bd80e5
JakeZook <jakezook01@gmail.com>
2024-02-03 13:17:42 -0700

    Atempt at fixing viewport bug for vertical tvs.

--- src/Components/ET.jsx
+++ src/Components/ET.jsx
@@ -1,38 +1,36 @@
 import React, { useEffect } from "react";
 
-//ET 5544
 const Leaderboard = () => {
 	useEffect(() => {
-		//Create script element
+		// Create container element
+		const container = document.createElement("div");
+		container.style.height = "85vh"; // Set container height to 50% of viewport height
+		container.style.overflow = "hidden"; // Hide overflow to display only the top half
+
+		// Create script element
 		const script = document.createElement("script");
 		script.async = true;
 		script.type = "text/javascript";
 		script.src = "https://www.escapekit.co/lbwidget.js?lid=5zlc9w06&room=5544";
 
-		//Append script to body and reload after 60 seconds
-		const intervalId = setInterval(() => {
-			//Check if script has already been appended
-			if (document.readyState === "complete") {
-				clearInterval(intervalId);
+		// Append script to container
+		container.appendChild(script);
 
-				document.body.appendChild(script);
+		// Append container to body
+		document.body.appendChild(container);
 
-				const reloadIntervalId = setInterval(() => {
-					window.location.reload();
-				}, 60000); // 60 seconds
+		const reloadIntervalId = setInterval(() => {
+			window.location.reload();
+		}, 60000); // 60 seconds
 
-				//Clear intervals and remove script from body
-				return () => {
-					clearInterval(reloadIntervalId);
-					document.body.removeChild(script);
-				};
-			}
-		}, 100);
-
+		// Clear intervals and remove container from body
 		return () => {
-			clearInterval(intervalId);
+			clearInterval(reloadIntervalId);
+			document.body.removeChild(container);
 		};
 	}, []);
+
+	return null; // Render nothing in the component, as the content is added dynamically.
 };
 
 export default Leaderboard;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<div className="bg-dark">
+			<App />
+		</div>
 	</React.StrictMode>
 );
