5402e7b6a7d35928f19900364f681b797c92f119
JakeZook <jakezook01@gmail.com>
2024-02-04 15:54:51 -0700

    Added logo to all rooms.

--- src/Components/SP.jsx
+++ src/Components/SP.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
+import Image from "/src/logo.png";
 
-//SP 5546
+//ET 5546
 const Leaderboard = () => {
 	useEffect(() => {
 		//Create main container
@@ -8,7 +9,7 @@ const Leaderboard = () => {
 		mainContainer.style.border = "none";
 		mainContainer.style.height = "80vh";
 		mainContainer.style.overflow = "hidden";
-		mainContainer.style.marginTop = "5vh";
+		mainContainer.style.marginTop = "3vh";
 
 		// Create widget container
 		const widgetContainer = document.createElement("div");
@@ -18,6 +19,12 @@ const Leaderboard = () => {
 		script.async = true;
 		script.type = "text/javascript";
 		script.src = "https://www.escapekit.co/lbwidget.js?lid=5zlc9w06&room=5546";
+
+		const img = document.createElement("img");
+		img.src = Image;
+		img.style.maxHeight = "8vh";
+		img.style.maxWidth = "26vh";
+		img.style.marginTop = "1vh";
 
 		// Append script to widget container
 		widgetContainer.appendChild(script);
@@ -34,15 +41,20 @@ const Leaderboard = () => {
 
 		// Create header element
 		const header = document.createElement("h1");
-		header.textContent = "Escape Rate: 41.9%";
+		header.textContent = "Escape Rate: 34.3%";
 		header.style.color = "white";
 		header.style.backgroundColor = "#181818";
 		header.style.textAlign = "center";
-		header.style.paddingTop = "4vh";
-		header.style.fontSize = "5rem";
+		header.style.paddingTop = "2vh";
+		header.style.marginBottom = "2vh";
+		header.style.fontSize = "3rem";
+		header.style.display = "flex";
+		header.style.flexDirection = "column";
+		header.style.alignItems = "center";
 
 		// Append header to body
 		document.body.appendChild(header);
+		header.appendChild(img);
 
 		// Clear intervals and remove main container and header from body
 		return () => {
