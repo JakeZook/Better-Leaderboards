f7a5e6ad98b388174ec1f5ce8ce62dc756534684
JakeZook <jakezook01@gmail.com>
2024-02-18 12:00:16 -0700

    Reverted time interval back.

--- src/Components/CH.jsx
+++ src/Components/CH.jsx
@@ -37,7 +37,7 @@ const Leaderboard = () => {
 
 		const reloadIntervalId = setInterval(() => {
 			window.location.reload();
-		}, 7200000); // 2 hours
+		}, 60000); // 60 seconds
 
 		// Create header element
 		const header = document.createElement("h1");
