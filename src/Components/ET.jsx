dd84ad80647ef45a2e7c28b3c433c42756f500db
JakeZook <jakezook01@gmail.com>
2024-02-19 14:28:00 -0700

    Egypt new widget test 2.

--- src/Components/ET.jsx
+++ src/Components/ET.jsx
@@ -1,69 +1,30 @@
 import React, { useEffect } from "react";
-import Image from "/src/logo.png";
 
-//ET 5544
 const Leaderboard = () => {
+	const widgetLink =
+		"https://www.escapekit.co/5zlc9w06/leaderboards/1/?cache=false&room=5544";
+	const escapeRate = "34.3%";
+
 	useEffect(() => {
-		//Create main container
-		const mainContainer = document.createElement("div");
-		mainContainer.style.border = "none";
-		mainContainer.style.height = "80vh";
-		mainContainer.style.overflow = "hidden";
-		mainContainer.style.marginTop = "3vh";
-
-		// Create widget container
-		const widgetContainer = document.createElement("div");
-
-		const widget = document.createElement("iframe");
-		widget.src =
-			"https://www.escapekit.co/5zlc9w06/leaderboards/1/?cache=false";
-		widget.style.width = "100%";
-
-		const img = document.createElement("img");
-		img.src = Image;
-		img.style.maxHeight = "8vh";
-		img.style.maxWidth = "26vh";
-		img.style.marginTop = "1vh";
-
-		// Append script to widget container
-		widgetContainer.appendChild(widget);
-
-		// Append widget container to main container
-		mainContainer.appendChild(widgetContainer);
-
-		// Append main container to body
-		document.body.appendChild(mainContainer);
-
 		const reloadIntervalId = setInterval(() => {
 			window.location.reload();
 		}, 60000); // 60 seconds
 
-		// Create header element
-		const header = document.createElement("h1");
-		header.textContent = "Escape Rate: 34.3%";
-		header.style.color = "white";
-		header.style.backgroundColor = "#181818";
-		header.style.textAlign = "center";
-		header.style.paddingTop = "2vh";
-		header.style.marginBottom = "2vh";
-		header.style.fontSize = "3rem";
-		header.style.display = "flex";
-		header.style.flexDirection = "column";
-		header.style.alignItems = "center";
-
-		// Append header to body
-		document.body.appendChild(header);
-		header.appendChild(img);
-
-		// Clear intervals and remove main container and header from body
+		// Clear intervals
 		return () => {
 			clearInterval(reloadIntervalId);
-			document.body.removeChild(mainContainer);
-			document.body.removeChild(header);
 		};
 	}, []);
 
-	return null; // Render nothing in the component, as the content is added dynamically.
+	return (
+		<div>
+			<iframe
+				src={widgetLink}
+				title="Embedded Content"
+				style={{ width: "100%", height: "800px", border: "none" }}
+			></iframe>
+		</div>
+	);
 };
 
 export default Leaderboard;
