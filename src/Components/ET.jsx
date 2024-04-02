5755a134e0a095dea25da7caaf3a6aca329beb1a
Jake Zook <132726907+JakeZook@users.noreply.github.com>
2024-04-02 16:32:02 -0600

    Update ET.jsx

--- src/Components/ET.jsx
+++ src/Components/ET.jsx
@@ -4,7 +4,7 @@ import Image from "/src/logo.png";
 const ET = () => {
 	const widgetLink =
 		"https://www.escapekit.co/5zlc9w06/leaderboards/1/?cache=false&room=5544";
-	const escapeRate = "34.3%";
+	const escapeRate = "35.3%";
 
 	useEffect(() => {
 		const reloadIntervalId = setInterval(() => {
